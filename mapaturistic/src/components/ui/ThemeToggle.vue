7dd747294bd11698529c7e2b1f6c227df666fcb2
milandxxx <santiagojoserhenalsguerra@gmail.com>
2026-04-08 20:24:59 -0500

    commit

--- mapaturistic/src/components/ui/ThemeToggle.vue
+++
@@ -1,7 +0,0 @@
-<template>
-<button @click='toggleTheme'>Tema</button>
-</template>
-
-<script setup>
-import { toggleTheme } from '../../store/themeStore'
-</script>
--- mapaturistic/src/utils/debounce.js
+++
@@ -1,1 +0,0 @@
-export const debounce=(fn,d=300)=>{let t;return(...a)=>{clearTimeout(t);t=setTimeout(()=>fn(...a),d)}}
