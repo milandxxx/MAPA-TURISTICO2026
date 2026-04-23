ff035c40f9512fc9f234b2d5b76b6e0361612707
milandxxx <santiagojoserhenalsguerra@gmail.com>
2026-04-23 05:38:57 -0500

    mamaa

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,19 +1,9 @@
 from pydantic import BaseModel
 
-class LugarBase(BaseModel):
-    nombre:str
-    lat:float
-    lon:float
-    descripcion:str
-    imagen:str
-    categoria:str
-    precio:float
-    rating:float
-
-class LugarCreate(LugarBase): pass
-
-class LugarResponse(LugarBase):
-    id:int
-    favorito:bool
-    class Config:
-        from_attributes=True
+class Lugar(BaseModel):
+    id: int
+    nombre: str
+    descripcion: str
+    precio: float
+    rating: float
+    imagen: str
