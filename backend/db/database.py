426d0be4e2be1aece72d088419a927da3078d7b0
hilcx <hilary.cortescar@gmail.com>
2026-05-20 19:58:28 -0500

    tercer cambio

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,19 +1,29 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "sqlite:///./lugares.db"
+DATABASE_URL = "mysql+pymysql://root:cbn2016@localhost/mapaturistico"
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
 def get_db():
+
     db = SessionLocal()
+
     try:
+
         yield db
+
     finally:
+
         db.close()
